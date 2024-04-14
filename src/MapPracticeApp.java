2f977c147ce01a802a63c2e208131274a5b38222
Zack Laine <rumtehk_uh@yahoo.com>
2024-04-14 18:01:20 -0400

    Experimenting with different map functions and slapping lambdas in em

--- src/MapPracticeApp.java
+++ src/MapPracticeApp.java
@@ -1,11 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MapPracticeApp {
     public static void main(String[] args) {
+        final String SOUGHT_HAND = "FLUSH";
         FileService fileService = new FileService();
-        List<String> pokerHands = fileService.populateSet(new ArrayList<>());
-//        pokerHands = fileService.populateSet(pokerHands);
+        List<String> pokerHandList = fileService.populateSet(new ArrayList<>());
+        pokerHandList = fileService.populateSet(pokerHandList);
+        Map<String, Integer> pokerMap = new HashMap<>();
+        for(String line:pokerHandList){
+            String player = line.split(",")[0];
+            String hand = line.split(",")[1];
+            if(pokerMap.containsKey(player)){
+                if(hand.contains(SOUGHT_HAND)){
+                    pokerMap.compute(player, (k, v) -> v + 1);
+                }
+            }
+            else{
+                if(hand.contains(SOUGHT_HAND)){
+                    pokerMap.put(player,1);
+                }
+                else{
+                    pokerMap.put(player,0);
+                }
 
+            }
+
+
+        }
+        System.out.println(pokerMap);
     }
 }
